03b8cff685075e918114e03d85fb261834e40da7
Andrei <andrei.massaini@hotmail.com>
2024-07-10 02:53:37 -0300

    first commit

---
+++ Materia.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class Materia {
+    // periodo/Nome materia/carga horaria/pre e co requisitos/Id da materia/
+    int periodo;
+    String nome;
+    int cargaHoraria;
+    ArrayList<String> preRequisitos;
+    ArrayList<String> coRequisitos;
+    String id;
+
+    public Materia() {
+    }
+
+}
