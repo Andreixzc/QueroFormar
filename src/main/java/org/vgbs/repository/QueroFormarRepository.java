d6d31698527d4a1ce4561a640677e9365f41624b
unknown <gustavopc2501@gmail.com>
2024-08-13 07:15:14 -0300

    tipagem

--- src/main/java/org/vgbs/repository/QueroFormarRepository.java
+++ src/main/java/org/vgbs/repository/QueroFormarRepository.java
@@ -6,6 +6,7 @@ import org.vgbs.dto.DisciplinaRestanteDTO;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
 import jakarta.transaction.Transactional;
 
 
@@ -16,27 +17,24 @@ public class QueroFormarRepository {
     
     @Transactional
     public List<DisciplinaRestanteDTO> findMissingDisciplinas(Long alunoId) {
-        String query = """
-            WITH MissingDisciplinas AS (
-            SELECT d.id AS disciplina_id, d.nome AS disciplina_nome
-            FROM disciplina d
-            JOIN curso_disciplina cd ON d.id = cd.disciplina_id
-            JOIN aluno_curso ac ON cd.curso_id = ac.curso_id
-            LEFT JOIN aluno_disciplina ad ON ad.disciplina_id = d.id AND ad.aluno_id = ac.aluno_id
-            WHERE ac.aluno_id = :alunoId
-            AND ad.disciplina_id IS NULL
-            )
-
-            SELECT md.disciplina_id, md.disciplina_nome, pr.disciplina_requisito_id, d2.nome AS requisito_nome,
-            horario.inicio, horario.fim
-            FROM MissingDisciplinas md
-            LEFT JOIN prerequisito pr ON md.disciplina_id = pr.disciplina_id
-            LEFT JOIN disciplina d2 ON pr.disciplina_requisito_id = d2.id
-            LEFT JOIN horario_disciplina ON horario_disciplina.disciplina_id = md.disciplina_id
-            LEFT JOIN horario ON horario_disciplina.horario_id = horario.id;
-                """;
-        return em.createNativeQuery(query, "DisciplinasRestantesMapeamento")
-                .setParameter("alunoId", alunoId)
-                .getResultList();
+        String jpqlQuery = """
+            SELECT new DisciplinaRestanteDTO(
+                d.id, d.nome, pr.disciplinaRequisito.id, d2.nome, h.inicio, h.fim)
+            FROM Disciplina d
+            JOIN CursoDisciplina cd ON d.id = cd.disciplina.id
+            JOIN AlunoCurso ac ON cd.curso.id = ac.curso.id
+            LEFT JOIN AlunoDisciplina ad ON ad.disciplina.id = d.id AND ad.aluno.id = ac.aluno.id
+            LEFT JOIN Prerequisito pr ON d.id = pr.disciplina.id
+            LEFT JOIN Disciplina d2 ON pr.disciplinaRequisito.id = d2.id
+            LEFT JOIN HorarioDisciplina hd ON hd.disciplina.id = d.id
+            LEFT JOIN Horario h ON hd.horario.id = h.id
+            WHERE ac.aluno.id = :alunoId
+            AND ad.id IS NULL
+        """;
+    
+        TypedQuery<DisciplinaRestanteDTO> query = em.createQuery(jpqlQuery, DisciplinaRestanteDTO.class);
+        query.setParameter("alunoId", alunoId);
+        
+        return query.getResultList();
     }
 }
