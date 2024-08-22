83b8fc58526b8c5331b9ab2dbc47d5dd95b5c190
gustavopcr <gustavopc2501@gmail.com>
2024-08-22 18:41:58 -0300

    grafo propriamente modelado

--- src/main/java/org/vgbs/service/GradeGrafo.java
+++ src/main/java/org/vgbs/service/GradeGrafo.java
@@ -2,10 +2,8 @@ package org.vgbs.service;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.Queue;
 
 import org.vgbs.dto.DisciplinaRestanteDTO;
 
@@ -16,49 +14,19 @@ public class GradeGrafo {
         this.listaAdj = new HashMap<>();
     }
 
-    public void printEntries(){
-        for (var tmp : this.listaAdj.entrySet()) {
-            System.out.println("entry: " + tmp.getValue());
-            for (var d : tmp.getValue()) {
-                System.out.println("dId: " + d.getDisciplinaId());
-                
-            }
+    public GradeGrafo(List<DisciplinaRestanteDTO> disciplinas){
+        this.listaAdj = new HashMap<>();
+        for(DisciplinaRestanteDTO d : disciplinas){
+            listaAdj.put(d.getDisciplinaId(), new ArrayList<>());
         }
     }
-    public void add(Long vertexId, DisciplinaRestanteDTO v){
-        List<DisciplinaRestanteDTO> vertex = listaAdj.get(vertexId);
 
-        if(vertex == null){
-            vertex = new ArrayList<>();
-            listaAdj.put(vertexId, vertex);
+    public void add(DisciplinaRestanteDTO v){
+        if(v.getDisciplinaRequisitoId() != null){
+            List<DisciplinaRestanteDTO> dependencias =  listaAdj.get(v.getDisciplinaRequisitoId());
+            if(dependencias != null){ // se == null, a disciplina ja foi concluida
+                dependencias.add(v);
+            } 
         }
-        vertex.add(v);
     } 
-
-    public void bfs(Long x){
-        Queue<Long> q = new LinkedList<>();
-        Map<Long, Boolean> visited = new HashMap<>();
-        Map<Long, Integer> distance = new HashMap<>();
-
-        visited.put(x, true);
-        distance.put(x, 0);
-        q.offer(x);
-
-        while(!q.isEmpty()){
-            Long s = q.poll(); 
-            List<DisciplinaRestanteDTO> disciplinas = listaAdj.get(s);
-            System.out.println("disciplinas: " + s + " with object: " + listaAdj.get(s));
-            System.out.println("Processing node: " + s + " with distance: " + distance.get(s));
-            if(disciplinas != null){
-                for (DisciplinaRestanteDTO d : disciplinas) {                    
-                    if( visited.get(d.getDisciplinaId()) != null && visited.get(d.getDisciplinaId()) )
-                        continue;
-                    visited.put(d.getDisciplinaId(), true);
-                    distance.put(d.getDisciplinaId(), distance.get(s)+1);
-                    q.offer(d.getDisciplinaId());
-                }
-            }
-        }
-
-    }
 }
--- src/main/java/org/vgbs/service/GrafoDependencia.java
+++ src/main/java/org/vgbs/service/GrafoDependencia.java
@@ -5,27 +5,24 @@ import java.util.List;
 
 import org.vgbs.dto.DisciplinaRestanteDTO;
 
-import com.google.common.collect.MultimapBuilder;
-import com.google.common.collect.SetMultimap;
-
 public class GrafoDependencia {
     
     
     public void montarGrafo(List<DisciplinaRestanteDTO> ld){
-        SetMultimap<Long, DisciplinaRestanteDTO> multimap = MultimapBuilder.hashKeys().hashSetValues().build();
         List<List<DisciplinaRestanteDTO>> grade = new ArrayList<>();
-        //Long firstId = ld.get(0).getDisciplinaId();
-        //System.out.println("firstId: " + firstId);
-        //System.out.println("firstNome: " + ld.get(0).getDisciplinaNome());
     
-        GradeGrafo g = new GradeGrafo();
+        GradeGrafo g = new GradeGrafo(ld);
         for (DisciplinaRestanteDTO d : ld) {
-            multimap.put(d.getDisciplinaRequisitoId(), d);
-            g.add( d.getDisciplinaRequisitoId()!=null ? d.getDisciplinaRequisitoId() : 0 , d);
+            g.add(d);
         }
-        //g.printEntries();
-
-        g.bfs(0L);
-       
+        
+        for(var entry : g.listaAdj.entrySet()){
+            Long key = entry.getKey();
+            List<DisciplinaRestanteDTO> valueList = entry.getValue();
+            System.out.println("Key: " + key);
+            for (var value : valueList) {
+                System.out.println("Disciplina Nome: " + value.getDisciplinaNome());
+            }
+        }
     }
 }
